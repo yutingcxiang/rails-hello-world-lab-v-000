010cc36bd33f2371bd4ef4275650415d476156fa
Christine Xiang <yutingcxiang@gmail.com>
2018-09-01 05:44:39 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  get 'about', to: 'static#about'
+  get 'hello_world', to: 'static#hello_world'
 end
